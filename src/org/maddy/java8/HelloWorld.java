2b4bccfb02884ec03239f036d8e4679e2bf7ba67
mputtegowda <madhucpsudan@gmail.com>
2016-11-21 14:09:35 +0530

    Hello world Committed

---
+++ src/org/maddy/java8/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.maddy.java8;
+
+/**
+* The HelloWorld program implements an application that
+* simply displays "Hello World!" to the standard output.
+*
+* @author  Madhusudan CP
+* @version 1.0
+* @since   2016-11-21 
+*/
+public class HelloWorld {
+
+   public static void main(String[] args) {
+      /* Prints Hello, World! on standard output. */
+      System.out.println("Hello World!");
+   }
+}
